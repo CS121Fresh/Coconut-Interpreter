412b5a501703a574ab329decf8ba0ef0343ad78c
James Ren <chrenbingyan@gmail.com>
2018-04-01 13:25:08 -0700

    changed domain for ajax

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,9 +10,6 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
-//= require trynode
-//= require console
-//= require posts
 //= require ace-rails-ap
 //= require rails-ujs
 //= require turbolinks
