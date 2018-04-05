0ec8bb3792094421514e3590c829e0ccfb44fe39
yuzhangClaremont <yzhang@students.pitzer.edu>
2018-04-05 01:37:14 -0700

    added linux database config option in comments, disabled yarn intergrity check in development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -9,6 +9,8 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  config.webpacker.check_yarn_integrity = false
+
   # Do not eager load code on boot.
   config.eager_load = false
 
